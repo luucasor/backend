5b954fee65404d58e76f87df157bea4814f184f7
luucasor <lucas@jornadatecnologia.com>
2023-08-25 10:56:00 -0300

    Backend conectando com banco mongodb ok

--- src/config/server.js
+++ src/config/server.js
@@ -1,10 +1,10 @@
 const port = 3003
-const bodyParser = require('body-parser')
+
 const express = require('express')
 const server = express()
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
